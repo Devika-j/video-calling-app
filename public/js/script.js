0bd3fdb85b9d5dd6eeb35dacac8fa5664198a7b3
Devika Jaiswal <devikaj2002@gmail.com>
2021-07-08 13:14:13 +0400

    port again

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:5000');
+const socket = io();
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
